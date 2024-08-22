320abca93901ad0abffa6d27db9b1ac80229988b
Dorin Bobea <Dorin.Bobea@dell.com>
2024-08-22 09:04:03 +0300

    Add project files.

---
+++ JokeService/Controllers/JokesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JokeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+     
+    }
+}
